1c0126f3b0953a53ce2932e1989a483dc579d0ce
tarena <tarena@app8deMac-mini.local>
2015-08-25 12:04:44 +0800

    GitTestUpdate

---
+++ Demo2_GitTest/GitTest.h
@@ -0,0 +1,13 @@
+//
+//  GitTest.h
+//  Demo2_GitTest
+//
+//  Created by tarena on 15/8/25.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GitTest : NSObject
+
+@end
---
+++ Demo2_GitTest/GitTest.m
@@ -0,0 +1,17 @@
+//
+//  GitTest.m
+//  Demo2_GitTest
+//
+//  Created by tarena on 15/8/25.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import "GitTest.h"
+
+@implementation GitTest
+
+-(void)test{
+    NSLog(@"gitTest");
+}
+
+@end
